e419683a7b68039f53a391ec7fadd786b32d67d2
Palak Neekhra <palakneekhra1234@gmail.com>
2025-01-15 19:41:03 +0530

    JavaScript

---
+++ Lecture_13/script.js
@@ -0,0 +1,51 @@
+// console.log("Hellooo World")
+
+//Datatypes
+// Number
+// String
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+
+// var a=10
+// a=100    Can be redeclared
+// console.log(a);
+
+// let a=20
+// let a=30   cannot redeclare
+// a=30
+// console.log(a);
+
+// // const a =110
+// const a    Cannot redeclare
+// a=10       cannot reassign
+// console.log(a);
+
+
+// var a =' rahul'
+// var b= 'jain'
+// console.log(a+' '+b);
+
+// console.log(`${a} ${b}`);
+
+
+//= == ===
+
+// console.log(5=='5');
+// console.log(5==='5');
+// console.log(false==='');
+// console.log(false=='');
+// console.log(false=='false');
+// console.log(false==='false');
+// console.log([]==[]);
+// console.log({}=={});
+// console.log([1]==[1]);
+// console.log({1:'Palak'}=={1:'Palak'});
+
+
+
+
+
+
---
+++ Lecture_14/index.js
@@ -0,0 +1,53 @@
+// let score=81
+
+// if(score>85)
+// {
+//     console.log("A");
+    
+// }
+// else if(score>75 && score<85){
+//     console.log("B");
+    
+// }
+// else if(score>35 && score<755){
+//     console.log("C");
+    
+// }
+// else{
+//     console.log("Fail");
+    
+// }
+
+// function sum(){
+//     console.log(5+5);
+    
+// }
+// sum()
+
+// const sum1=()=>{
+//     console.log(10+10);
+    
+// }
+// sum1()
+
+// function sum(a=10,b=10){
+//     console.log(a+b);
+    
+// }
+// sum(1,2)
+
+// function sum(){
+//     // console.log();
+//     return "hello"
+// }
+// console.log(sum());
+
+let arr = [1,"hello",true,null,undefined]
+for(let i in arr){
+    console.log(i);
+    
+}
+for(let i of arr){
+    console.log(i);
+    
+}
