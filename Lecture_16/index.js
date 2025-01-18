fe13083e73152ac3698bc8e8988960624f60b3c3
Palak Neekhra <palakneekhra1234@gmail.com>
2025-01-18 19:47:24 +0530

    JS

---
+++ Lecture_16/index.js
@@ -0,0 +1,23 @@
+// console.log(a);
+// var a =10;
+
+// outer()
+// function outer(){
+//     console.log("Hiiii");
+    
+// }
+// sum()
+// var sum=()=>{
+//     console.log("hello");
+    
+// }
+
+// sum()
+// let sum=()=>{
+//     console.log("Hellooo");
+    
+// }
+
+console.log(false==[]);
+console.log(false==![]);
+
